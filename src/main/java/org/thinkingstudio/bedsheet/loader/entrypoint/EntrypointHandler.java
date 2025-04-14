f4c4991b8216730b43ebda01ebd50bbd6644f7b5
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-04-14 22:42:13 +0800

    add `ClientModInitializer`

--- src/main/java/org/thinkingstudio/bedsheet/loader/entrypoint/EntrypointHandler.java
+++ src/main/java/org/thinkingstudio/bedsheet/loader/entrypoint/EntrypointHandler.java
@@ -17,6 +17,10 @@ public class EntrypointHandler {
                     Optional<DedicatedServerModInitializer> serverModInitializer = modContainer.getCustomExtension(DedicatedServerModInitializer.class);
                     serverModInitializer.ifPresent(DedicatedServerModInitializer::onInitializeServer);
                 }
+                if (FMLLoader.getDist().isClient()) {
+                    Optional<ClientModInitializer> clientModInitializer = modContainer.getCustomExtension(ClientModInitializer.class);
+                    clientModInitializer.ifPresent(ClientModInitializer::onInitializeClient);
+                }
             });
         });
     }
