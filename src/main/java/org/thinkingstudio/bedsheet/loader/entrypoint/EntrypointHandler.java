9b34be11ed1c7c9fe44cffa04fbb1ca8611fab9f
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-04-14 23:39:13 +0800

    clean

--- src/main/java/org/thinkingstudio/bedsheet/loader/entrypoint/EntrypointHandler.java
+++ src/main/java/org/thinkingstudio/bedsheet/loader/entrypoint/EntrypointHandler.java
@@ -1,23 +1,27 @@
 package org.thinkingstudio.bedsheet.loader.entrypoint;
 
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModList;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.neoforged.fml.loading.FMLLoader;
 
 import java.util.Optional;
 
 public class EntrypointHandler {
-    public static void init() {
-        ModList.get().forEachModContainer((modId, modContainer) -> {
-            Optional<ModInitializer> modInitializer = modContainer.getCustomExtension(ModInitializer.class);
-            modInitializer.ifPresent(ModInitializer::onInitialize);
-            if (FMLLoader.getDist().isDedicatedServer()) {
-                Optional<DedicatedServerModInitializer> serverModInitializer = modContainer.getCustomExtension(DedicatedServerModInitializer.class);
-                serverModInitializer.ifPresent(DedicatedServerModInitializer::onInitializeServer);
-            }
-            if (FMLLoader.getDist().isClient()) {
-                Optional<ClientModInitializer> clientModInitializer = modContainer.getCustomExtension(ClientModInitializer.class);
-                clientModInitializer.ifPresent(ClientModInitializer::onInitializeClient);
-            }
+    public static void init(IEventBus modEventBus) {
+        modEventBus.addListener(FMLLoadCompleteEvent.class, event -> {
+            ModList.get().forEachModContainer((modId, modContainer) -> {
+                Optional<ModInitializer> modInitializer = modContainer.getCustomExtension(ModInitializer.class);
+                modInitializer.ifPresent(ModInitializer::onInitialize);
+                if (FMLLoader.getDist().isDedicatedServer()) {
+                    Optional<DedicatedServerModInitializer> serverModInitializer = modContainer.getCustomExtension(DedicatedServerModInitializer.class);
+                    serverModInitializer.ifPresent(DedicatedServerModInitializer::onInitializeServer);
+                }
+                if (FMLLoader.getDist().isClient()) {
+                    Optional<ClientModInitializer> clientModInitializer = modContainer.getCustomExtension(ClientModInitializer.class);
+                    clientModInitializer.ifPresent(ClientModInitializer::onInitializeClient);
+                }
+            });
         });
     }
 }
